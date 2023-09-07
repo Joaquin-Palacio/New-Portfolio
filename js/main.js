66df9cc39bf1f0077ebf44179c7eb5dce4744da1
Joaquin <joaquinpalacio2311@gmail.com>
2023-09-07 18:55:09 -0300

    scroll listo

--- js/main.js
+++ js/main.js
@@ -65,9 +65,40 @@ const sendEmail = (e) => {
       },
       () => {
         // Show error message
-        contactMessage.textContent = ' Message not sent (service error) ❌'
+        contactMessage.textContent = ' Message not sent (service error) ❌';
       }
     );
 };
 
 contactForm.addEventListener('submit', sendEmail);
+
+/* ========== SHOW SCROLL UP ========== */
+const scrollUp = () => {
+  const scrollUp = document.getElementById('scroll-up');
+  this.scrollY >= 350
+    ? scrollUp.classList.add('show-scroll')
+    : scrollUp.classList.remove('show-scroll');
+};
+window.addEventListener('scroll', scrollUp);
+
+/* ========== SCROLL SECTIONS ACTIVE LINK ========== */
+const sections = document.querySelectorAll('section[id]');
+
+const scrollActive = () => {
+  const scrollDown = window.scrollY;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute('id'),
+      sectionClass = document.querySelector(
+        '.nav__menu a[href*=' + sectionId + ']'
+      );
+      if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+        sectionClass.classList.add('active-link')
+      } else {
+        sectionClass.classList.remove('active-link')
+      }
+  });
+};
+window.addEventListener('scroll', scrollActive)
