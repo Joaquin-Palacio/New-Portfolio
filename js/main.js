0d8acb0e6bcdf36ec31777c563a8f078ec70a28f
Joaquin <joaquinpalacio2311@gmail.com>
2023-09-07 17:00:15 -0300

    emailjs agregado y terminado form de contacto

--- js/main.js
+++ js/main.js
@@ -33,4 +33,41 @@ const shadowHeader = () => {
     ? header.classList.add('shadow-header')
     : header.classList.remove('shadow-header');
 };
-window.addEventListener('scroll', shadowHeader)
+window.addEventListener('scroll', shadowHeader);
+
+/* ========== EMAIL JS ========== */
+const contactForm = document.getElementById('contact-form');
+contactMessage = document.getElementById('contact-message');
+
+const sendEmail = (e) => {
+  e.preventDefault();
+
+  // serviceID - templateID - #form - publicKey
+  emailjs
+    .sendForm(
+      'service_3wvz7nx',
+      'template_qyagnsr',
+      '#contact-form',
+      '_8jBlctSspVmQys4o'
+    )
+    .then(
+      () => {
+        // Show sent message
+        contactMessage.textContent = 'Message sent successfully ✅';
+
+        // Remove message after five seconds
+        setTimeout(() => {
+          contactMessage.textContent = '';
+        }, 5000);
+
+        // Clear input fields
+        contactForm.reset();
+      },
+      () => {
+        // Show error message
+        contactMessage.textContent = ' Message not sent (service error) ❌'
+      }
+    );
+};
+
+contactForm.addEventListener('submit', sendEmail);
